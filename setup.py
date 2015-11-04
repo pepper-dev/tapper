f94351c8a7a524f1ed89524ad7aa7ac5b6c12250
honey-trap15 <tkgm15h@gmail.com>
2015-11-05 00:13:38 +0900

    setup.py修正

--- setup.py
+++ setup.py
@@ -19,14 +19,15 @@ install_requires=[
     ]
 
 setup(name='tapper',
+    keywords='tapper',
     version='0.1',
     description='Pepper tablet scafflod create tools.',
     author='Takegami Hiroyuki, Minabe Yoji, Yamamoto',
+    license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=install_requires,
     entry_points={
         'console_scripts':
-            'tapper = tapper.scripts.tapper:main'
-    },
-    keywords='tapper'
-    )
+            'tapper=tapper.scripts.tapper:main'
+    })
